32662995626de5445305dfa2339c8bcd7ec96cd4
ss9324558 <115810209+ss9324558@users.noreply.github.com>
2024-01-08 22:33:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.getElementById('openModal').addEventListener('click', function() {
+  document.getElementById('myModal').style.display = 'block';
+});
+
+document.querySelector('.close-modal').addEventListener('click', function() {
+  document.getElementById('myModal').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target === document.getElementById('myModal')) {
+    document.getElementById('myModal').style.display = 'none';
+  }
+});
